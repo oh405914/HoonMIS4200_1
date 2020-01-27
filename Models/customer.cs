3e7047d948afd4a0cb47dc9a67e89572207863e3
Olivia Hoon <oh405914@ohio.edu>
2020-01-27 10:35:29 -0500

    customer changes

---
+++ Models/customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoonMIS4200.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+        public int customerLastName { get; set; }
+        public int customerFirstName { get; set; }
+        public int email { get; set; }
+        public int phone { get; set; }
+        public DateTime customerSince { get; set; }
+    }
+}
