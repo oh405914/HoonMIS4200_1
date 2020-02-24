08270048fc41887fbe28625622cc4c33db0a92f6
Olivia Hoon <oh405914@ohio.edu>
2020-02-24 10:32:56 -0500

    Name changes

--- Models/Course.cs
+++ Models/Course.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HoonMIS4200.Models
 {
     public class Course
     {
         public int courseID { get; set; }
+        [Display (Name ="Course Description")]
         public string description { get; set; }
+        [Display(Name = "Course Date")]
         public DateTime courseDate { get; set; }
         // add any other fields as appropriate
         //Order is on the "one" side of a one-to-many relationship with OrderDetail
         //and we indicate that with an ICollection
+        [Display(Name = "Course Details")]
         public ICollection<CourseDetail> CourseDetail { get; set; }
         //Order is on the Many side of the one-to-many relation between Customer
         //and Order and we represent that relationship like this
